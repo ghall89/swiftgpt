94acb097996ae98894ad8be7395708b8799f8867
Graham Hall <graham@ghall.dev>
2023-03-25 15:51:39 -0400

    Initial Commit

---
+++ SwiftGPT/SwiftGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGPTApp.swift
+//  SwiftGPT
+//
+//  Created by Graham Hall on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
